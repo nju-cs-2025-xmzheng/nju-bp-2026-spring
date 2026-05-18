f6ace4cc2dfc64ad43ed866b649aadfe41b417af
sprite <ximingzheng@smail.nju.edu.cn>
2026-05-19 00:37:07 +0800

    Add random number generation functionality with initialization methods

---
+++ src/random.c
@@ -0,0 +1,26 @@
+#include "random.h"
+#include <stdlib.h>
+#include <time.h>
+
+static int initialized = 0;
+
+void init_rand_without_seed() {
+    if (initialized)
+        return;
+    srand((unsigned int)time(NULL));
+    initialized = 1;
+}
+
+void init_rand_with_seed(unsigned int seed) {
+    if (initialized)
+        return;
+    srand(seed);
+    initialized = 1;
+}
+
+int randint(int min, int max) {
+    if (!initialized)
+        init_rand();
+    double scale = (double)rand() / (double)RAND_MAX;
+    return min + (int)(scale * ((double)max - (double)min));
+}
